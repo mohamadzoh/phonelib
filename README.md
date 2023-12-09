93a47973745d9b875f242f1b49f9811740f996ed
Mohamad Al Zohbie <mohamad.zohbie@sowlutions.com>
2023-12-09 22:56:16 +0200

    add documentation for the code

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
- # Phonelib
- 
+# Phonelib
+
 Phonelib is a Rust library for handling phone numbers. It provides functions for validation, cleaning, and extracting country information from phone numbers.
 
 ## Usage
+
 Add this to your `Cargo.toml`:
-~~~
+
+```
 [dependencies]
 phonelib = "0.1.0"
-~~~
+```
+
 ## Struct
-~~~
+
+```
 pub struct Country {
 pub name: &'static str,
 pub code: &'static str,
@@ -17,12 +21,19 @@ pub phone_lengths: &'static [u8],
 pub prefix: u32,
 }
 
-~~~
+```
+
+## USAGE
+
+```
+extern crate phonelib
+```
+
 ## EXAMPLE
 
- - validating if phone number is valid 
+- validating if phone number is valid
 
-~~~
+```
 let  phone_number  =  "+96179123123".to_string();
 
 if  phonelib::is_valid_phone_number(&phone_number) {
@@ -30,9 +41,11 @@ if  phonelib::is_valid_phone_number(&phone_number) {
 } else {
 	println!("{} is not a valid phone number.", phone_number);
 }
-~~~
+```
+
 - extracting country code and information about phone number
-~~~
+
+```
 let  phone_number  =  "+11231231232".to_string();
 match  phonelib::extract_country(&phone_number) {
 Some(country) => {
@@ -40,17 +53,21 @@ println!("Country code for {} is: {}", phone_number, country.code);
 }
 None => println!("Unable to extract country information for {}", phone_number),
 }
-~~~
-- get santinized phone number 
-~~~
+```
+
+- get santinized phone number
+
+```
 let  phone_number  =  "+096179123123".to_string();
 match  phonelib::normalize_phone_number(&phone_number) {
 Some(cleaned) => println!("Cleaned phone number: {}", cleaned),
 None => println!("Invalid characters in the phone number."),
 }
-~~~
+```
+
 - santinize phone number in place (while affect the value of the passed string)
-~~~
+
+```
 // Example 3: Cleaning a phone number in place
 
 let  mut  phone_number  =  "+0012345678912".to_string();
@@ -58,7 +75,7 @@ match  phonelib::normalize_phone_number_in_place(&mut  phone_number) {
 Some(_) => println!("Cleaned phone number in place: {}", phone_number),
 None => println!("Invalid characters in the phone number."),
 }
-~~~
+```
 
 ## Rusty Rails Project
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,12 @@ mod definitions;
 mod tests;
 
 pub fn is_valid_phone_number(phone_number: String) -> bool {
+    // check if the phone number contains invalid character
     if contains_invalid_character(&phone_number) {
         return false;
     }
+
+    // normalize the phone number and check if it is valid or not
     normalize_phone_number(phone_number).is_some()
 }
 
@@ -17,14 +20,16 @@ pub fn extract_country(phone_number: String) -> Option<&'static Country> {
     remove_unwanted_character(&mut phone_number);
     extract_country_data(&phone_number)
 }
+
 pub fn normalize_phone_number(phone_number: String) -> Option<String> {
+    // make a copy of phone number and normalize it in place and return it
     normalize_phone_number_in_place(&mut phone_number.clone())
 }
 
 pub fn normalize_phone_number_in_place(phone_number: &mut String) -> Option<String> {
     remove_unwanted_character(phone_number);
-    println!("xxxxxx{}xxxxxxxx", phone_number);
-    // Extrpub fn extract_country(phone_number: &mut String) -> Option<>act country data
+
+    // extract country data
     let country = extract_country_data(&phone_number)?;
 
     // Remove country code from phone number
@@ -33,7 +38,7 @@ pub fn normalize_phone_number_in_place(phone_number: &mut String) -> Option<Stri
     // Remove all leading zeros if present
     leading_zero_remover(phone_number);
 
-    // Add country code again
+    // Add country code again to the phone number and return it
     let normalize_phone_number = format!("+{}{}", country.prefix, phone_number);
 
     Some(normalize_phone_number)
@@ -48,6 +53,7 @@ fn remove_unwanted_character(phone_number: &mut String) {
 
 fn contains_invalid_character(phone_number: &String) -> bool {
     let mut parentheses_count = 0;
+    // check if the phone number contains invalid character
 
     for c in phone_number.chars() {
         match c {
@@ -64,16 +70,19 @@ fn contains_invalid_character(phone_number: &String) -> bool {
 
 
 fn remove_non_digit_character(phone_number: &mut String) {
+    // remove all non digit character
     phone_number.retain(|c| c.is_numeric());
 }
 
 fn leading_zero_remover(phone_number: &mut String) {
+    // remove all leading zeros
     while phone_number.starts_with('0') {
         phone_number.remove(0);
     }
 }
 
 fn extract_country_data(phone_number: &str) -> Option<&'static Country> {
+    // check if the phone number starts with country code or not and return country data if found
     for country in COUNTRIES.iter() {
         if phone_number.starts_with(&country.prefix.to_string()) {
             if country
